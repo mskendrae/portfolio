927c964d18514644672c113be28d21181a029697
mskendrae <mskendrae@gmail.com>
2018-05-08 15:46:15 -0400

    styling of the portfolio

--- js/script.js
+++ js/script.js
@@ -53,6 +53,15 @@ $(document).ready(function(){
             });
 
             $("[data-fancybox]").fancybox();
+            $(".items").isotope({
+              filter: '*',
+              animationOptions:{
+                 duration: 1500,
+                 easing: 'linear',
+                 queue: false,
+              }
+
+            })
 
 
 });
