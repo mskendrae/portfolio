d202f752a016765416a4331f94fa1418d6a13c92
mskendrae <mskendrae@gmail.com>
2018-05-08 16:42:39 -0400

    adding of navbar, copyright

--- js/script.js
+++ js/script.js
@@ -60,8 +60,46 @@ $(document).ready(function(){
                  easing: 'linear',
                  queue: false,
               }
+            });
 
-            })
+            $("#filters a").click(function(){
 
+              $("#filters .current").removeClass("current");
+              $(this).addClass("current");
+
+            var selector = $(this).attr("data-filter");
+
+                      $(".items").isotope({
+                        filter: selector,
+                        animationOptions:{
+                           duration: 1500,
+                           easing: 'linear',
+                           queue: false,
+                        }
+                    });
+
+                    return false;
+          });
+
+
+          const nav = $("#navigation");
+          const navTop = nav.offset().top;
+
+          $(window).on("scroll", stickyNavigation);
+
+          function stickyNavigation() {
+
+            var body = $("body");
+
+            if($(window).scrollTop() >= navTop) {
+              body.css("padding-top", nav.outerHeight() + "px");
+              body.addClass("fixedNav");
+            }
+            else {
+              body.css("padding-top", 0);
+              body.removeClass("fixedNav");
+              }
+
+              }
 
 });
